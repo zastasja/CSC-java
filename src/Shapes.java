356737f8f6011099dcb597dbc9b32422b393dc16
zastasja <anastasia_zh@bk.ru>
2022-04-26 19:04:52 +0300

    updated Shapes class

--- src/Shapes.java
+++ src/Shapes.java
@@ -1,10 +1,10 @@
-public class Shapes {
+public abstract class Shapes {
     private final Color color;
     public Shapes(Color color) {
         this.color = color;
     }
 
     public Color getColor(){return color;}
-    public double getArea() {return Double.NaN;}
+    public abstract double getArea();
 }
 
