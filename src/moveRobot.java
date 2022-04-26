4f3f8e8e22bdaf07398ff140826e9df97ccb837d
zastasja <anastasia_zh@bk.ru>
2022-04-26 15:47:37 +0300

    solved move Robot task

---
+++ src/moveRobot.java
@@ -0,0 +1,77 @@
+
+public class moveRobot {
+    public enum Direction {
+        UP,
+        DOWN,
+        LEFT,
+        RIGHT
+    }
+    public class Robot {
+
+        public Direction getDirection() {
+            // текущее направление взгляда
+        }
+
+        public int getX() {
+            // текущая координата X
+        }
+
+        public int getY() {
+            // текущая координата Y
+        }
+
+        public void turnLeft() {
+            // повернуться на 90 градусов против часовой стрелки
+        }
+
+        public void turnRight() {
+            // повернуться на 90 градусов по часовой стрелке
+        }
+
+        public void stepForward() {
+            // шаг в направлении взгляда
+            // за один шаг робот изменяет одну свою координату на единицу
+        }
+    }
+    public static void moveRobot(Robot robot, int toX, int toY) {
+        int currentX = robot.getX();
+        int currentY = robot.getY();
+        int stepsX = toX - currentX;
+        int stepsY = toY - currentY;
+        int stepBackX = Math.abs(toX - currentX);
+        int stepBackY = Math.abs(toY - currentY);
+
+        while (robot.getDirection() != Direction.RIGHT){
+            robot.turnRight();
+        }
+        if(stepsX > 0){
+            for(int i = 0; i < stepsX; i++){
+                robot.stepForward();
+            }
+        } else if (stepsX < 0) {
+            robot.turnLeft();
+            robot.turnLeft();
+            for(int i = 0; i < stepBackX; i++ ){
+                robot.stepForward();
+            }
+        }
+
+        while (robot.getDirection() != Direction.UP){
+                robot.turnLeft();
+            }
+        if (stepsY > 0){
+            for(int j = 0; j < stepsY; j++){
+                robot.stepForward();
+            }
+        } else if (stepsY < 0) {
+            robot.turnLeft();
+            robot.turnLeft();
+            for (int j=0; j < stepBackY; j++){
+                robot.stepForward();
+            }
+        }
+
+
+        robot.stepForward(); // your implementation here
+    }
+}
