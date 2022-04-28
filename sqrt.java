f623ea3a1ed855c3620d66db984947c4f36e699a
Nastasja <33484571+zastasja@users.noreply.github.com>
2022-04-28 18:16:36 +0300

    Delete sqrt.java

--- sqrt.java
+++
@@ -1,6 +0,0 @@
-public class sqrt {
-    public static double sqrt(double x) {
-        if(x<0) throw new IllegalArgumentException("Expected non-negative number, got"+x);
-        return Math.sqrt(x);
-    }
-}
