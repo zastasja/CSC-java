4cbac118a059f2cad7419feb55b09a1716c9fd26
Nastasja <33484571+zastasja@users.noreply.github.com>
2022-04-28 11:11:29 +0300

    sqrt with exception

---
+++ sqrt.java
@@ -0,0 +1,6 @@
+public class sqrt {
+    public static double sqrt(double x) {
+        if(x<0) throw new IllegalArgumentException("Expected non-negative number, got"+x);
+        return Math.sqrt(x);
+    }
+}
