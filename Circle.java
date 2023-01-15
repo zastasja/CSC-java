44532ac32ebd2fc32915d7335930a0a042241e34
Nastasja <33484571+zastasja@users.noreply.github.com>
2023-01-15 14:28:11 +0300

    Update Circle.java

--- Circle.java
+++ Circle.java
@@ -6,10 +6,13 @@ public class Circle {
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter radius: ");
         double r = sc.nextDouble();
+        if (r >= 0) {
+            double area = Math.PI*Math.pow(r, 2);
 
-        double area = Math.PI*Math.pow(r, 2);
-
-        System.out.println("For the circle with radius: " + r + ";");
-        System.out.println("the area is: "+ area + ".");
+            System.out.println("For the circle with radius: " + r + ";");
+            System.out.println("the area is: "+ area + ".");
+        } else {
+            System.out.println("Radius must be positive");
+        }
     }
 }
