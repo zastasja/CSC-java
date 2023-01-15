f17dfc8dcf0994be396d3823f30773c5365c9221
Nastasja <33484571+zastasja@users.noreply.github.com>
2023-01-15 14:27:41 +0300

    Update CircleTest.java

--- CircleTest.java
+++ CircleTest.java
@@ -10,7 +10,7 @@ import  org.junit.Test;
 
 public class CircleTest {
     @Test
-    public void circleTest() throws IOException {
+    public void circleTest1() throws IOException {
         String input = "1\n";
 
         ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
@@ -24,6 +24,59 @@ public class CircleTest {
         String consoleOutput = "Enter radius: " + System.lineSeparator();
         consoleOutput += "For the circle with radius: 1.0;" + System.lineSeparator();
         consoleOutput += "the area is: 3.141592653589793." + System.lineSeparator();
+
+        assertEquals(consoleOutput, out.toString());
+    }
+    @Test
+    public void circleTest3() throws IOException {
+        String input = "3\n";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        String[] args = {};
+        Circle.main(args);
+
+        String consoleOutput = "Enter radius: " + System.lineSeparator();
+        consoleOutput += "For the circle with radius: 3.0;" + System.lineSeparator();
+        consoleOutput += "the area is: 28.274333882308138." + System.lineSeparator();
+
+        assertEquals(consoleOutput, out.toString());
+    }
+    @Test
+    public void circleTest0() throws IOException {
+        String input = "0\n";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        String[] args = {};
+        Circle.main(args);
+
+        String consoleOutput = "Enter radius: " + System.lineSeparator();
+        consoleOutput += "For the circle with radius: 0.0;" + System.lineSeparator();
+        consoleOutput += "the area is: 0.0." + System.lineSeparator();
+
+        assertEquals(consoleOutput, out.toString());
+    }
+    @Test
+    public void circleTestNegative() throws IOException {
+        String input = "-3\n";
+
+        ByteArrayInputStream in = new ByteArrayInputStream(input.getBytes());
+        System.setIn(in);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        System.setOut(new PrintStream(out));
+
+        String[] args = {};
+        Circle.main(args);
+
+        String consoleOutput = "Enter radius: " + System.lineSeparator();
+        consoleOutput += "Radius must be positive" + System.lineSeparator();
 
         assertEquals(consoleOutput, out.toString());
     }
